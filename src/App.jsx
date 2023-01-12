51c0ce942e6907647d9acde9569da253b55da323
Alann <alanntapia209@gmail.com>
2023-01-11 21:29:24 -0800

    First MileStone

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import About from './components/about/About.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Services />
+    <Portfolio />
+    <Testimonials />
+    <Contact />
+    <Footer />
+    </>
+  )
+}
+
+export default App
