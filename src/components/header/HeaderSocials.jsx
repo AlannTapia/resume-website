d7992dbf62a7f37cca03d8d6bd97b46a0ca73b30
Alann <atapia24@ucmerced.edu>
2023-01-20 00:42:12 -0800

    Fixed Icons

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -2,19 +2,32 @@ import React from 'react'
 import './HeaderSocials.css'
 import GIT from '../../assets/icons8-github-50.png'
 import Linked from '../../assets/icons8-linkedin-50.png'
+import Twitter from '../../assets/icons8-twitter-50.png'
+import Insta from '../../assets/icons8-instagram-50.png'
 
 const HeaderSocials = () => {
-  /*console.log('HeaderSocials component is rendered') */
-  console.log(HeaderSocials)
   return (
-    <div className='hs'>
+    <div className='hs-container'>
+      <div className='hs'>
         <a href='https://github.com' target="_blank" rel="noreferrer">
-            <img src={GIT} />
+          <img src={GIT} alt = 'Hi' />
         </a>
-        
-        <a href='linkedin.com' target="_blank" rel="noreferrer">
-            <img src={Linked} />
+      </div>
+      <div className='hs'>
+        <a href='https://linkedin.com' target="_blank" rel="noreferrer">
+          <img src={Linked} alt = 'Hi'/>
         </a>
+      </div>
+      <div className='hs'>
+        <a href='https://twitter.com' target="_blank" rel="noreferrer">
+          <img src={Twitter}alt = 'Hi' />
+        </a>
+      </div>
+      <div className='hs'>
+        <a href='https://instagram.com' target="_blank" rel="noreferrer">
+          <img src={Insta} alt = 'Hi' />
+        </a>
+      </div>
     </div>
   )
 }
