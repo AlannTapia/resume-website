ecedb841dda84f8e6145767c99a606795b5fd444
Alann <atapia24@ucmerced.edu>
2023-02-06 14:42:06 -0800

    Finished about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,8 +11,8 @@ const About = () => {
       <h5>Get To Know </h5>
       <h2> About Me</h2>
 
-      <div className='container about__container'>
-        <div className='about__me'>
+      <div className="container about__container">
+        <div className="about__me">
           <div className='about__me-image'>
             <img src={ME} alt='About' />
           </div>
